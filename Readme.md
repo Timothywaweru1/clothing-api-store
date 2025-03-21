42bae556b38f47efda2a7dc536ecc608518636c7
Timothywaweru1 <timothy.waweru1@student.moringaschool.com>
2025-03-21 21:28:07 +0300

    final api for clothing store

---
+++ Readme.md
@@ -0,0 +1,51 @@
+// POST && PATCH 
+
+// let baseUrl = "http://localhost:3000/article";
+
+// fetch (baseUrl, {
+//     method : "POST",
+//     headers : {
+//         "Content-Type" : "application/json"
+//     },
+//     body : JSON.stringify ({key : value})
+// })
+// .then (res => res.json)
+// .then (data => console.log(data)) 
+// .catch(error => console.log (error))
+
+// /*DELETE*/
+// fetch (baseUrl, {
+//     method : "DELETE",
+// })
+/*
+CRUD =>
+    CREATE = POST
+READ = GET
+UPDATE = PATCH
+DELETE
+*/// POST && PATCH 
+
+// let baseUrl = "http://localhost:3000/article";
+
+// fetch (baseUrl, {
+//     method : "POST",
+//     headers : {
+//         "Content-Type" : "application/json"
+//     },
+//     body : JSON.stringify ({key : value})
+// })
+// .then (res => res.json)
+// .then (data => console.log(data)) 
+// .catch(error => console.log (error))
+
+// /*DELETE*/
+// fetch (baseUrl, {
+//     method : "DELETE",
+// })
+/*
+CRUD =>
+    CREATE = POST
+READ = GET
+UPDATE = PATCH
+DELETE
+*/
--- js/index.js
+++ js/index.js
@@ -1,46 +1,48 @@
-
+let baseUrl = "http://localhost:3000/products"
 function displayProducts (products) {
     let productCatalog = document.querySelector ("#productCatalog")
      products.forEach (product => {
         let html = ` 
         <div class="singleProduct">
-                <h5>${product.title}</h5>
+                <h5>${product.title.substring (1,20)}</h5>
                 <img src="${product.image}" alt="bag">
-                <p><strong>${product.description}</strong>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                <p><strong>${product.description.substring (1,30)}...</strong>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
                 <div><strong>${product.price}</strong></div>
                 <div><strong>${product.category}</strong></div>
+                <div>
+                <button class = "update">update</button>
+                <button class = "delete">delete</button>
         </div>`
             productCatalog.innerHTML += html
     })
      }
-fetch ("https://fakestoreapi.com/products")
+fetch (baseUrl)
 .then (response => response.json ())
 .then ( data => displayProducts (data))
 .catch ((error) => console.log (error));
 
-// POST && PATCH 
+function deleteProduct () {
 
-// let baseUrl = "http://localhost:3000/article";
+}
+function updateProduct () {
 
-// fetch (baseUrl, {
-//     method : "POST",
-//     headers : {
-//         "Content-Type" : "application/json"
-//     },
-//     body : JSON.stringify ({key : value})
-// })
-// .then (res => res.json)
-// .then (data => console.log(data)) 
-// .catch(error => console.log (error))
+}
+function postProduct () { 
+    let form = document.querySelector ("form")
+    form.addEventListener ("submit", (event )=> {
+        event.preventDefault()
+        let productTitle = document.querySelector ("#title").value
+        let productImage = document.querySelector ("#image").value
+        let productPrice = document.querySelector ("#price").value
+        let productDescription = document.querySelector ("#description").value
+    
+        let productObject = {
+            title : productTitle,
+            image : productImage,
+            price : productPrice,
+            description : productDescription,
+        }
+        console.log(productObject)
+    })
 
-// /*DELETE*/
-// fetch (baseUrl, {
-//     method : "DELETE",
-// })
-/*
-CRUD =>
-    CREATE = POST
-READ = GET
-UPDATE = PATCH
-DELETE
-*/
+}
